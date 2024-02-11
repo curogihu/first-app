c1d073c9b7a7850850e02afd7223cf8bb9bb8884
curogihu <curogihu@users.noreply.github.com>
2024-02-11 14:24:59 +0900

    how to show characters I inputed to brower  at this time

--- react-crash-course/src/components/NewPost.jsx
+++ react-crash-course/src/components/NewPost.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  // const stateData = useState('');
+  // stateData[0] // current value
+  // stateData[1] // state updaing function
+  //
+
+  const [ enteredBody, setEnteredBody ] = useState(''); 
+  // let enteredBody = '';
+
   function changeBodyHandler (event) {
     // console.log(event.target.value);
+    // enteredBody = event.target.value;
+    setEnteredBody(event.target.value);
   }
 
   return (
@@ -11,6 +22,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
