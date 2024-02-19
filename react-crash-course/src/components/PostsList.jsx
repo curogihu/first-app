b47085f01aa19a4d21af066f1da93468459cb767
curogihu <curogihu@users.noreply.github.com>
2024-02-19 13:52:20 +0900

    outputting list data

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -21,10 +21,18 @@ function PostsList({isPosting, onStopPosting}) {
                     />
                 </Modal>
             )}
-
-            <ul className={ classes.posts }>
-                {posts.map((post) => <Post author={post.author} body={post.body} />)}
-            </ul>
+            {posts.length > 0 && (
+                <ul className={ classes.posts }>
+                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+                </ul>
+            )}
+            {posts.length === 0 && (
+                <div style={{textAlign: 'center'}}>
+                    <h2>There are no posts yet.</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+            
         </>
     );
 }
