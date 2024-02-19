2f3842ac43893f74de32f9595546a99fc7276499
curogihu <curogihu@users.noreply.github.com>
2024-02-19 22:14:20 +0900

    Handle Loading State

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -7,13 +7,16 @@ import classes from './PostsList.module.css';
 
 function PostsList({isPosting, onStopPosting}) {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8080/posts');
             const resData = await response.json();
-            console.log(resData)
+            // console.log(resData)
             setPosts(resData.posts);
+            setIsFetching(false);
         }
         
         fetchPosts();
@@ -39,18 +42,22 @@ function PostsList({isPosting, onStopPosting}) {
                     />
                 </Modal>
             )}
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={ classes.posts }>
                     {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>
             )}
-            {posts.length === 0 && (
+            {!isFetching && posts.length === 0 && (
                 <div style={{textAlign: 'center'}}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>
             )}
-            
+            {isFetching && (
+                <div style={{textAlign: 'center', color: 'white'}}>
+                    <p>Loading posts...</p>
+                </div>
+            )}
         </>
     );
 }
